98f8cb81dfd685aa0213a27db092675764bf6b27
dimi3ryi <feschenko.dmitryi@gmail.com>
2017-06-26 21:17:47 +0300

    initial commit

---
+++ src/test/java/com/technoboom/meetingapp/MeetingAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.technoboom.meetingapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MeetingAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
